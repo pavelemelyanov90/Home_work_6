9283b1fb39972ee4c4c067b0bffdc978d2a74299
Pavel <kwadrakilla666@gmail.com>
2023-02-03 17:39:22 +0300

    final_version

---
+++ Example_41/Program.cs
@@ -0,0 +1,101 @@
+﻿// Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// Вариант с рандомным заполнением
+
+/*Console.WriteLine("Введите количество чисел:\t");
+int col = int.Parse(Console.ReadLine()!);
+
+int[] InputArray (int size, int minValue, int maxValue)
+{
+    int[] arr = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(minValue, maxValue + 1 );
+    }
+    return arr;
+}
+
+
+int OutArray(int[] result)
+{
+    int count = 0;
+    for(int i = 0; i < result.Length; i++)
+    {
+        if(result[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] array = InputArray(col, 0, 10);
+Console.WriteLine(String.Join(" , ", array));
+int res = OutArray(array);
+Console.WriteLine($"Количество значений больше 0 равно: {res});*/
+
+// Вариант с пользовательским вводом
+
+int[] StringToNum(string input)
+{
+    int count = 1;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] == ',')
+        {
+            count++;
+        }
+    }
+
+    int[] numbers = new int [count];
+    int index = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        string temp = String.Empty;
+
+        while (input [i] != ',')
+        {
+        if(i != input.Length - 1)
+        {
+            temp += input [i].ToString();
+            i++;
+        }
+        else
+        {
+            temp += input [i].ToString();
+            break;
+        }
+        }
+        numbers[index] = Convert.ToInt32(temp);
+        index++;
+    }
+    return numbers;
+}
+
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " , ");
+    }
+    Console.Write("]");
+}
+
+Console.Clear();
+Console.Write("Введите числа через запятую: ");
+int[] numbers = StringToNum(Console.ReadLine());
+PrintArray(numbers);
+int sum = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > 0)
+    {
+        sum++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"количество значений больше 0 = {sum}");
---
+++ Example_43/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+
+Console.WriteLine("Ведите k1:\t");
+double num1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Ведите b1:\t");
+double num2 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Ведите k2:\t");
+double num3 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Ведите b2:\t");
+double num4 = double.Parse(Console.ReadLine()!);
+
+double x = (num2 - num4)/(num3 - num1); // x = (b1-b2)/(k2-k1)
+double y = (num3 * num2 - num1 * num4) / (num1 - num3); // y = (k2*b1-k1*b2)/(k2-k1)
+if(num1 == num3)
+{
+    Console.WriteLine("Прямые параллельны или совпадают");
+}
+else
+{
+    Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
+}
+
